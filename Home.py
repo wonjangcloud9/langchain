3081b8db78502a87e10e67ef6829eac7dd7d2f2c
wonjangcloud9 <venuscwj@naver.com>
2024-04-02 14:50:37 +0900

    documentGPT Introduction

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Hello world!")
+
+st.write("This is a simple Streamlit app.")
+
+st.write("This is a simple Streamlit app.")
+
+st.markdown("This is a simple Streamlit app.")
