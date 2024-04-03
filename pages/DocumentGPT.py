e92fa06e64284f48fbf6b2f4579f3c83c73c08f8
wonjangcloud9 <venuscwj@naver.com>
2024-04-03 22:12:57 +0900

    코드 정리

--- pages/DocumentGPT.py
+++ pages/DocumentGPT.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import UnstructuredFileLoader
@@ -11,7 +12,6 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 from dotenv import load_dotenv
-import re
 
 load_dotenv()
 
@@ -58,12 +58,8 @@ llm = ChatOpenAI(
 template = ChatPromptTemplate.from_messages(
     [
         (
-            "system",
-            """
-            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
-
-            Context: {context}
-            """,
+            "system", "Answer the question using ONLY the following context. "
+                      "If you don't know the answer just say you don't know. DON'T make anything up. Context: {context}",
         ),
         ("human", "{question}"),
     ]
@@ -75,8 +71,10 @@ def embed_file(file):
     file_content = file.read()
     file_path = f"./..cache/files/{file.name}"
     os.makedirs(f"./..cache/files/", exist_ok=True)
+
     with open(file_path, "wb") as f:
         f.write(file_content)
+
     os.makedirs(f"./..cache/embeddings", exist_ok=True)
     cache_dir = LocalFileStore(f"./..cache/embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
@@ -90,6 +88,7 @@ def embed_file(file):
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
+
     return retriever
 
 
@@ -138,6 +137,7 @@ with st.sidebar:
 
 with st.sidebar:
     api_key = st.text_input("OPENAI_API_KEY를 넣어야 작동합니다.").strip()
+
     if api_key:
         save_api_key(api_key)
         st.write("API_KEY가 저장되었습니다.")
@@ -145,16 +145,20 @@ with st.sidebar:
         st.session_state["api_key"] = api_key
 
     button = st.button("저장")
+
     if button:
         save_api_key(api_key)
+
         if api_key == "":
             st.write("API_KEY를 넣어주세요.")
+
 if (st.session_state["api_key_bool"] == True) and (st.session_state["api_key"] != None):
     if file:
         retriever = embed_file(file)
         send_message("I'm ready! Ask away!", "ai", save=False)
         paint_history()
         message = st.chat_input("Ask anything about your file")
+
         if message:
             if re.match(pattern, st.session_state["api_key"]):
                 send_message(message, "human")
@@ -162,6 +166,7 @@ if (st.session_state["api_key_bool"] == True) and (st.session_state["api_key"] !
                             "context": retriever | RunnableLambda(format_docs),
                             "question": RunnablePassthrough(),
                         } | template | llm
+
                 with st.chat_message("ai"):
                     resposne = chain.invoke(message)
             else:
@@ -172,5 +177,4 @@ if (st.session_state["api_key_bool"] == True) and (st.session_state["api_key"] !
 
 with st.sidebar:
     st.write("Made by Wonjang")
-
     st.write("https://github.com/wonjangcloud9/langchain/blob/main/pages/DocumentGPT.py")
