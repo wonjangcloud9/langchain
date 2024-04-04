2fbe2d10b430e467b1c29bd9061520045dc4b40a
wonjangcloud9 <venuscwj@naver.com>
2024-04-05 08:11:58 +0900

    Formatter Prompt

--- pages/QuizGPT.py
+++ pages/QuizGPT.py
@@ -21,67 +21,15 @@ llm = ChatOpenAI(
 )
 
 
-@st.cache_resource(show_spinner="Loading file..")
-def split_file(file):
-    file_content = file.read()
-    file_path = f"./.cache/quiz_files/{file.name}"
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
-    )
-    loader = UnstructuredFileLoader(file_path)
-    docs = loader.load_and_split(text_splitter=splitter)
-    return docs
-
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
 
-
-
-with st.sidebar:
-    docs = None
-    choice = st.selectbox(
-        "Choose what you want to use.",
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
         (
-            "File",
-            "Wikipedia Article",
-        ),
-    )
-    if choice == "File":
-        file = st.file_uploader(
-            "Upload a .docx , .txt or .pdf file",
-            type=["pdf", "txt", "docx"],
-        )
-        if file:
-            docs = split_file(file)
-            st.write(docs)
-    else:
-        topic = st.text_input("Search Wikipedia...")
-        if topic:
-            retriever = WikipediaRetriever(top_k_results=5, lang="ko")
-            with st.status("Searching Wikipedia..."):
-                docs = retriever.get_relevant_documents(topic)
-
-if not docs:
-    st.markdown(
-        """
-    Welcome to QuizGPT.
-
-    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
-
-    Get started by uploading a file or searching on Wikipedia in the sidebar.
-    """
-    )
-else:
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                """
+            "system",
+            """
     You are a helpful assistant that is role playing as a teacher.
 
     Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
@@ -108,13 +56,202 @@ else:
 
     Context: {context}
 """,
-            )
+        )
+    ]
+)
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a powerful formatting algorithm.
+
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+
+    Example Input:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+
+    Example Output:
+
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
         ]
+     }}
+    ```
+    Your turn!
+
+    Questions: {context}
+
+""",
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
+
+@st.cache_resource(show_spinner="Loading file..")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+
+with st.sidebar:
+    docs = None
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
     )
 
-    chain = {"context": format_docs} | prompt | llm
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
 
+        if file:
+            docs = split_file(file)
+            st.write(docs)
+    else:
+        topic = st.text_input("Search Wikipedia...")
+
+        if topic:
+            retriever = WikipediaRetriever(top_k_results=5, lang="ko")
+            with st.status("Searching Wikipedia..."):
+                docs = retriever.get_relevant_documents(topic)
+
+if not docs:
+    st.markdown(
+        """
+    Welcome to QuizGPT.
+
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
+    )
+
+else:
     start = st.button("Generate Quiz")
 
     if start:
-        chain.invoke(docs)
+        questions_response = questions_chain.invoke(docs)
+        st.write(questions_response.content)
+        formatting_response = formatting_chain.invoke(
+            {"context": questions_response.content}
+        )
+        st.write(formatting_response.content)
